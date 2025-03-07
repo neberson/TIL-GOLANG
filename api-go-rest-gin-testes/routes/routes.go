944deb0e0e90ed9c6491089ec6e102b1fd057262
Neberson Andrade <neberson_mtv@hotmail.com>
2025-03-06 22:36:28 -0300

    - Implementado EndPoint para servir página HTML

--- api-go-rest-gin-testes/routes/routes.go
+++ api-go-rest-gin-testes/routes/routes.go
@@ -8,11 +8,15 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.GET("/index", controllers.ExibePaginaIndex)
+	r.NoRoute(controllers.RotaNaoEncontrada)
 	r.Run(":8000")
 }
